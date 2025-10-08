9cc64036d4b1cec4f074c0fab36bc4e28811df51
jordanklaers <jordan.klaers@gmail.com>
2025-10-08 09:20:42 -0600

    updates footer company info

--- src/pages/components/CompanyInfo.vue
+++ src/pages/components/CompanyInfo.vue
@@ -4,7 +4,8 @@
 <template>
   <div className="container-company-info">
     <span>brightlandholidaylights@gmail.com</span>
-    <span>Brightland Legal/Insurance</span>
+    <span>Brightland is Bonded and Insured</span>
+    <span>206-483-4726</span>
     <span>
       <i class="fa-solid fa-location-pin"></i>
       Serving the Seattle area
